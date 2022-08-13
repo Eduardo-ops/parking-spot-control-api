61c7dbc786bd5e089dde83cd01eb226708348d8d
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-08-13 17:22:50 -0300

    Implementado os metodos para listar, listar por id, deletar e atualizar vagas.

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -3,18 +3,16 @@ package com.api.parkingcontrol.controllers;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
@@ -22,41 +20,85 @@ import com.api.parkingcontrolDto.ParkingSpotDto;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600) // Permite que seja acessado de qualquer fonte.
-@RequestMapping("/parking-sport")
+@RequestMapping("/parking-spot")
 public class ParkingSpotController {
 
-	// @Autowired
-	// ParkingSportService parkingSportService;
+    // @Autowired
+    // ParkingSportService parkingSportService;
 
-	final ParkingSpotService parkingSportService;
+    final ParkingSpotService parkingSpotService;
 
-	public ParkingSpotController(ParkingSpotService parkingSportService) {
-		super();
-		this.parkingSportService = parkingSportService;
-	}
+    public ParkingSpotController(ParkingSpotService parkingSportService) {
+        super();
+        this.parkingSpotService = parkingSportService;
+    }
 
-	@PostMapping
-	public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
-		if (parkingSportService.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License Plate Car is already in use!");
-		}
-		if (parkingSportService.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking spot is already in use!");
-		}
-		if (parkingSportService.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())) {
-			return ResponseEntity.status(HttpStatus.CONFLICT)
-					.body("Conflict: Parking Spot already registered in apartment and block!");
-		}
+    @GetMapping
+    public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots() {
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    }
 
-		ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
-		BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
-		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
-		return ResponseEntity.status(HttpStatus.CREATED).body(parkingSportService.save(parkingSpotModel));
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("parking Spot not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
 
-	@GetMapping
-	public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots() {
-		return ResponseEntity.status(HttpStatus.OK).body(parkingSportService.findAll());
-	}
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        if (parkingSpotService.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License Plate Car is already in use!");
+        }
+        if (parkingSpotService.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking spot is already in use!");
+        }
+        if (parkingSpotService.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered in apartment and block!");
+        }
 
+        ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id, @RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+
+        ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
+
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+        parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+
+        // Segunda forma de fazer o update
+//        ParkingSpotModel parkingSpotModel = parkingSpotModelOptional.get();
+//
+//        parkingSpotModel.setParkingSpotNumber(parkingSpotDto.getParkingSpotNumber());
+//        parkingSpotModel.setLicensePlateCar(parkingSpotDto.getLicensePlateCar());
+//        parkingSpotModel.setModelCar(parkingSpotDto.getModelCar());
+//        parkingSpotModel.setBrandCar(parkingSpotDto.getBrandCar());
+//        parkingSpotModel.setColorCar(parkingSpotDto.getColorCar());
+//        parkingSpotModel.setResponsibleName(parkingSpotDto.getResponsibleName());
+//        parkingSpotModel.setApartment(parkingSpotDto.getApartment());
+//        parkingSpotModel.setBlock(parkingSpotDto.getBlock());
+
+        parkingSpotService.save(parkingSpotModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Parking Spot updated successfully!!!");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> removeParkingSpot(@PathVariable(value = "id") UUID id) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("parking Spot not found.");
+        }
+        parkingSpotService.delete(parkingSpotModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully");
+    }
 }
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.api.parkingcontrol.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import javax.transaction.Transactional;
 
@@ -12,35 +14,43 @@ import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 @Service
 public class ParkingSpotService {
 
-	// Ponto de injeção para o ParkingSportRepository
-	// @Autowired
-	// ParkingSportRepository parkingSportRepository;
+    // Ponto de injeção para o ParkingSportRepository
+    // @Autowired
+    // ParkingSportRepository parkingSportRepository;
 
-	final ParkingSpotRepository parkingSportRepository;
+    final ParkingSpotRepository parkingSportRepository;
 
-	public ParkingSpotService(ParkingSpotRepository parkingSportRepository) {
-		this.parkingSportRepository = parkingSportRepository;
-	}
+    public ParkingSpotService(ParkingSpotRepository parkingSportRepository) {
+        this.parkingSportRepository = parkingSportRepository;
+    }
 
-	@Transactional
-	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
-		return parkingSportRepository.save(parkingSpotModel);
-	}
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSportRepository.save(parkingSpotModel);
+    }
 
-	public List<ParkingSpotModel> findAll() {
-		return parkingSportRepository.findAll();
-	}
+    public List<ParkingSpotModel> findAll() {
+        return parkingSportRepository.findAll();
+    }
 
-	public boolean existsByLicensePlateCar(String licensePlateCar) {
-		return parkingSportRepository.existsByLicensePlateCar(licensePlateCar);
-	}
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSportRepository.existsByLicensePlateCar(licensePlateCar);
+    }
 
-	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
-		return parkingSportRepository.existsByParkingSpotNumber(parkingSpotNumber);
-	}
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSportRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
 
-	public boolean existsByApartmentAndBlock(String apartment, String block) {
-		return parkingSportRepository.existsByApartmentAndBlock(apartment, block);
-	}
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSportRepository.existsByApartmentAndBlock(apartment, block);
+    }
 
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return this.parkingSportRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        this.parkingSportRepository.delete(parkingSpotModel);
+    }
 }
